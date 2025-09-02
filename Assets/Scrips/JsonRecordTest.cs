f7d9a7e59c83f4e366a9f124aca07234fe50cc3d
Seo Jun Gyu <127666839+SeoJunGyu@users.noreply.github.com>
2025-09-02 23:17:03 +0900

    녹화 및 재생

--- Assets/Scrips/JsonRecordTest.cs
+++ Assets/Scrips/JsonRecordTest.cs
@@ -4,6 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+public class Record
+{
+    public Vector3 position;
+    public Quaternion rotation;
+}
 
 public class JsonRecordTest : MonoBehaviour
 {
@@ -12,37 +20,129 @@ public class JsonRecordTest : MonoBehaviour
 
     public GameObject target;
 
-    List<SaveDatas> saves = new List<SaveDatas>();
+    List<Record> saves = new List<Record>();
 
     public float interval = 1f;
+    private float timer = 0f;
 
     public bool IsRecord { get; set; }
+
+    public GameObject startReplay;
+    public GameObject stopReplay;
+
+    Coroutine replayHandle;
 
     private void FixedUpdate()
     {
         
     }
 
-    public void Record()
+    public void RecordData()
     {
         IsRecord = !IsRecord;
         if (IsRecord)
         {
-
+            StartCoroutine(CoRecord());
         }
         else
         {
-            var json = JsonConvert.SerializeObject(saves, Formatting.Indented, new Vector3Converter());
+            StopCoroutine(CoRecord());
+
+            var json = JsonConvert.SerializeObject(saves, Formatting.Indented, new Vector3Converter(), new QuaternionConverter());
             File.WriteAllText(FileFullPath, json); //포지션 저장
         }
             
     }
 
-    public void Replay()
+    public void LoadReplayData()
     {
 
         var json = File.ReadAllText(FileFullPath);
-        var result = JsonConvert.DeserializeObject<List<SaveDatas>>(json, new Vector3Converter(), new QuaternionConverter(), new ColorConverter());
+        saves = JsonConvert.DeserializeObject<List<Record>>(json, new Vector3Converter(), new QuaternionConverter());
+    }
+
+    public void StartReplay()
+    {
+        startReplay.SetActive(false);
+        stopReplay.SetActive(true);
+
+        if(saves == null || saves.Count == 0)
+        {
+            LoadReplayData();
+        }
+
+        if(replayHandle != null)
+        {
+            StopCoroutine(replayHandle);
+        }
+
+        replayHandle = StartCoroutine(CoReplay());
+    }
+
+    public void StopReplay()
+    {
+        startReplay.SetActive(true);
+        stopReplay.SetActive(false);
+
+        if (replayHandle != null)
+        {
+            StopCoroutine(replayHandle);
+            replayHandle = null;
+        }
+    }
+
+    public IEnumerator CoRecord()
+    {
+        while (true)
+        {
+            timer += Time.deltaTime;
+            if(timer > interval)
+            {
+                var tmp = new Record()
+                {
+                    position = target.transform.position,
+                    rotation = target.transform.rotation
+                };
+                saves.Add(tmp);
+
+                timer = 0f;
+            }
+            yield return null;
+        }
+    }
+
+    public IEnumerator CoReplay()
+    {
+        target.transform.SetPositionAndRotation(saves[0].position, saves[0].rotation);
+        if(saves.Count == 1)
+        {
+            replayHandle = null;
+            yield break;
+        }
+
+        for (int i = 0; i < saves.Count - 1; i++)
+        {
+            var a = saves[i];
+            var b = saves[i + 1];
+
+            timer = 0f;
+            while(timer < interval)
+            {
+                timer += Time.deltaTime / interval;
+                if(timer > interval)
+                {
+                    timer = 1f;
+                }
+
+                var pos = Vector3.Lerp(a.position, b.position, timer);
+                var rot = Quaternion.Slerp(a.rotation, b.rotation, timer);
+                target.transform.SetPositionAndRotation(pos, rot);
+            }
+            
+            yield return new WaitForSeconds(interval);
+        }
+
+        replayHandle = null;
     }
 
     
